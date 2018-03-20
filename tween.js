c8a10100ca77c777cfea480f021de3a883118a62
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-20 15:54:50 -0400

    C00l stu44

--- tween.js
+++ tween.js
@@ -1,16 +1,43 @@
 window.onload = function() {
-  console.log('asdlf')
-  const one = document.getElementById("one");
-  TweenLite.to("#one", 1, { 
+  TweenLite.to("#top", 1, { 
     opacity: 1
   })
 }
 
-$(function() {
+$(document).ready(function() {
   $('a[href*="#"]')
     .not('[href="#"]')
     .not('[href="#0"]')
     .click(function (event) {
-      console.log(event)
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+        if (target.length) {
+          event.preventDefault();
+
+          TweenLite.to($('html, body'), 1.5, {
+            scrollTop: target.offset().top,
+            onComplete: function() {
+              $(target).focus();
+              if ($(target).is(":focus")) {
+                return false;
+              } else {
+                $(target).attr('tabindex', '-1');
+                $(target).focus();
+              }
+            }
+          })
+
+          TweenLite.to($(target), 2.5, {
+            opacity: 1
+          })
+
+          
+        }
+      }
     })
 })
